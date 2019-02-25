5af6fc197326fc878b49529bc4c155a0d9f5fcfb
Chen <fangjian_chen@jbcc.co.jp>
2019-02-25 11:37:43 +0900

    Updates

---
+++ precache-manifest.ab0b083badec40fa80d3a424df6153e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/intro/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "392a15f6e7fe91f02bea",
+    "url": "/intro/static/js/runtime~main.392a15f6.js"
+  },
+  {
+    "revision": "bc4d809f6e78381f09ea",
+    "url": "/intro/static/js/main.bc4d809f.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/intro/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "bc4d809f6e78381f09ea",
+    "url": "/intro/static/css/main.0ad19404.chunk.css"
+  },
+  {
+    "revision": "cc936f4b5905532725ebc688bacf56c5",
+    "url": "/intro/index.html"
+  }
+];
